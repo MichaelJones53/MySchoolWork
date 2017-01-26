c47679003a2d3eb8313f2aeeb2196b87c21cb65f
Michael Jones <mykejonez53@gmail.com>
2017-01-26 14:25:04 -0800

    Initial Commit for Lab1 - Fizzbuzz
    Created Fizzbuzz program with fizzBuzzOutput method.  added
    documentation as well.

---
+++ Lab1 - Fizzbuzz!/src/edu/miracosta/cs113/Fizzbuzz.java
@@ -0,0 +1,47 @@
+package edu.miracosta.cs113;
+
+/**
+ * Fizzbuzz.java : Fizzbuzz counter program
+ * 
+ * @author Michael Jones
+ * @version 1.0
+ *
+ */
+public class Fizzbuzz {
+	/**
+	 * Tester for random assistant theory checker
+	 * 
+	 * @param args
+	 *            command line arguments (unnused)
+	 * @param NUMBER_CAP
+	 * 		      constant defined for number being counted to		
+	 */
+	public static void main(String[] args) {
+		final int NUMBER_CAP = 100;
+		
+		fizzBuzzOutput(NUMBER_CAP);
+		
+	}
+	/**
+	 * Will count from 1 to NUMBER_CAP, printing the number to the screen.
+	 * if number is multiple of 15, prints "Fizzbuzz" instead.  If number 
+	 * is multiple of 5, prints "Buzz" instead.  If number is multiple 
+	 * of 3, prints "Fizz" instead.  
+	 * 
+	 * @param num
+	 *            number to be counted up to
+	 */
+	public static void fizzBuzzOutput(int num){
+		for(int i=0; i< num; i++){
+			if((i+1) % 15 == 0){
+				System.out.println("Fizzbuzz");
+			}else if((i+1) % 5 == 0){
+				System.out.println("Buzz");
+			}else if((i+1) % 3 == 0){
+				System.out.println("Fizz");
+			}else{
+				System.out.println(i+1);
+			}
+		}
+	}
+}
---
+++ Lab1 - Fizzbuzz!/src/edu/miracosta/cs113/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Contains source code for Lab1 -Fizzbuzz demo
+ */
+/**
+ * @author W7201584
+ *
+ */
+package edu.miracosta.cs113;

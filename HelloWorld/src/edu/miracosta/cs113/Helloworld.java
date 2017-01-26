b613f30fd86d6562e5c59e4a39178692d963feb6
Michael Jones <mykejonez53@gmail.com>
2017-01-25 17:15:00 -0800

    Initial Commit Hello World
    test GitHub commit + push, print to screen "Hello, World"

---
+++ HelloWorld/src/edu/miracosta/cs113/Helloworld.java
@@ -0,0 +1,11 @@
+package edu.miracosta.cs113;
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+		System.out.println("hello, world");
+		
+	}
+
+}
+
---
+++ HelloWorld/src/edu/miracosta/cs113/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Contains source code for Hello World demo
+ */
+/**
+ * @author w7201584
+ *
+ */
+package edu.miracosta.cs113;

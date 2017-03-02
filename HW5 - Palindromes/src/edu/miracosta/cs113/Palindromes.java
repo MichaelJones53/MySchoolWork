9e191b4d26f5b5a71308a1a64bc0d2332cc08ba3
Michael Jones <mykejonez53@gmail.com>
2017-03-01 16:41:37 -0800

    Initial Commit HW5
    Initial commith HW5.  set up files.

---
+++ HW5 - Palindromes/src/edu/miracosta/cs113/Palindromes.java
@@ -0,0 +1,10 @@
+package edu.miracosta.cs113;
+
+public class Palindromes {
+
+	
+	public static void main(String[] args){
+		
+		
+	}
+}
---
+++ HW5 - Palindromes/src/edu/miracosta/cs113/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Contains class for HW 5 Palindromes
+ */
+/**
+ * @author w7201584
+ *
+ */
+package edu.miracosta.cs113;

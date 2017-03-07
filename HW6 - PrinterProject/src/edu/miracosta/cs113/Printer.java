6997c906ed15866c61c688be032c1ac411c90730
Michael Jones <mykejonez53@gmail.com>
2017-03-07 15:26:44 -0800

    Commit HW6
    Commit HW6.  added Jobs class to Printer class

--- HW6 - PrinterProject/src/edu/miracosta/cs113/Printer.java
+++ HW6 - PrinterProject/src/edu/miracosta/cs113/Printer.java
@@ -4,15 +4,62 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class Printer {
+	private static int time = 0;
 	private int mininumPages;
 	private int maxinumPages;
 	private Queue<Job> jobQueue = new LinkedList<Job>();
 	
-
 	
 	
 	
 	private static class Job{
+		private int pages;
+		private int submitTime;
+		private int startTime;
+		private int completedTime;
+		
+		public Job(int pages){
+			this.pages = pages;
+			submitTime = time;
+			startTime = 0;
+			completedTime = 0;
+		}
+
+		public int getPages() {
+			return pages;
+		}
+
+		public void setPages(int pages) {
+			this.pages = pages;
+		}
+
+		public int getSubmitTime() {
+			return submitTime;
+		}
+
+		public void setSubmitTime(int submitTime) {
+			this.submitTime = submitTime;
+		}
+
+		public int getStartTime() {
+			return startTime;
+		}
+
+		public void setStartTime(int startTime) {
+			this.startTime = startTime;
+		}
+
+		public int getCompletedTime() {
+			return completedTime;
+		}
+
+		public void setCompletedTime(int completedTime) {
+			this.completedTime = completedTime;
+		}
+		
+		public String toString(){
+			return "Number of Pages: "+pages+"\tSubmit time: " +submitTime+"\tCompleted Time: "+completedTime; 
+		}
 		
 		
 	}

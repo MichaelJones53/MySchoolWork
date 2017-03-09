813eae85c4d10f6aa801e942073ee60b8e9524b5
Michael Jones <mykejonez53@gmail.com>
2017-03-09 07:40:18 -0800

    Commit HW6
    driver functional.  need documentation.iver.  functional for single
    printer and test dataset of 6 items.

--- HW6 - PrinterProject/src/edu/miracosta/cs113/PrinterDriver.java
+++ HW6 - PrinterProject/src/edu/miracosta/cs113/PrinterDriver.java
@@ -1,32 +1,80 @@
 package edu.miracosta.cs113;
 
+import java.security.KeyStore.Entry;
+import java.util.Scanner;
+
 public class PrinterDriver {
+	private static int[] testArray;
+	private static int MAX_PAGES = 50;
 
 	public static void main(String[] args){
+		String numPrinters = "";
+		boolean isEntering = true;
+		Scanner keyboard = new Scanner(System.in);
+		Printer smallPrinter = new Printer("smallPrinter", 1, Integer.MAX_VALUE);
+		Printer mediumPrinter = new Printer("mediumPrinter", 1, Integer.MAX_VALUE);
+		Printer largePrinter = new Printer("largePrinter", 1, Integer.MAX_VALUE);
 		
+		generateTestArray(100);
 		
-		int[] testArray = {5,15,30,22,11, 7};
-		Printer printer = new Printer("testPrinter", 1, 100);
-		
-		for(int i = 0;i<6;i++){
-			printer.addJob(testArray[i]);
-			printer.printNextJob();
-			
-			Printer.incrimentTime();
-			
-			
-			
+		while(isEntering){
+			System.out.println("Enter the number of printers 1-3");
+			numPrinters = keyboard.nextLine();
+			if(numPrinters.equals("1") || numPrinters.equals("2") || numPrinters.equals("3")){
+				isEntering = false;
+			}else{
+				System.out.println("!!!!!!!!!!Invalid entry!!!!!!");
+			}
 		}
+
+		if(numPrinters.equals("3")){
+			for(int i = 0;i<testArray.length;i++){
+				if(testArray[i]<=10){
+					smallPrinter.addJob(testArray[i]);
+				}else if(testArray[i]<=20){
+					mediumPrinter.addJob(testArray[i]);
+				}else{
+					largePrinter.addJob(testArray[i]);
+				}
+				
+				smallPrinter.printNextJob();
+				mediumPrinter.printNextJob();
+				largePrinter.printNextJob();
+				Printer.incrimentTime();
+			}
+		}else if(numPrinters.equals("2")){
+			for(int i = 0;i<testArray.length;i++){
+				if(testArray[i]<=10){
+					smallPrinter.addJob(testArray[i]);
+				}else{
+					mediumPrinter.addJob(testArray[i]);
+				}
+				
+				smallPrinter.printNextJob();
+				mediumPrinter.printNextJob();
+				Printer.incrimentTime();
+			}
+		}else{
+			for(int i = 0;i<testArray.length;i++){
+				smallPrinter.addJob(testArray[i]);
+				smallPrinter.printNextJob();
+				Printer.incrimentTime();
+			}
 		
-		while(printer.hasNextJob()){
-			printer.printNextJob();
-			
-			Printer.incrimentTime();
-			
-			
-			
 		}
-		
+		while(smallPrinter.hasNextJob() || mediumPrinter.hasNextJob() || largePrinter.hasNextJob()){
+			smallPrinter.printNextJob();
+			mediumPrinter.printNextJob();
+			largePrinter.printNextJob();
+			Printer.incrimentTime();
+		}
+	}
+	
+	private static void generateTestArray(int size){
+		testArray = new int[size];
+		for(int i=0; i<testArray.length;i++){
+			testArray[i] = (int) Math.ceil(Math.random()*MAX_PAGES);
+		}
 		
 		
 	}

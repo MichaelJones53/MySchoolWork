19a77bd3c5a5a596db814bcc4b689efd17093d6d
Michael Jones <mykejonez53@gmail.com>
2017-03-09 11:15:38 -0800

    Commit HW6 Printer project
    completed printer project.    functional for single
    printer and test dataset of 6 items.

--- HW6 - PrinterProject/src/edu/miracosta/cs113/Printer.java
+++ HW6 - PrinterProject/src/edu/miracosta/cs113/Printer.java
@@ -11,23 +11,46 @@ public class Printer {
 	private boolean isPrinting = false;
 	private Queue<Job> jobQueue = new LinkedList<Job>();
 	
+	/**
+	 * Constructor.  instantiates printer object with provided parameters
+	 * @param name
+	 * 		the name of the printer
+	 * @param minPages
+	 * 		the mininum number of pages
+	 * @param maxPages
+	 * 		the maximum number of pages
+	 */
 	public  Printer(String name, int minPages, int maxPages){
 		this.name = name;
 		mininumPages = minPages;
 		maximumPages = maxPages;
 	}
 	
-	
+	/**
+	 * adds job to printer queue
+	 * 
+	 * @param pages 
+	 * 			the number of pages of the job being added to the queue
+	 */
 	public void addJob(int pages){
 		Job newJob = new Job(pages);
 		jobQueue.add(newJob);
 		System.out.println(name+":  added JOB "+newJob.pages+" pages at TIME: "+time);
 	}
 	
+	/**
+	 * returns whether or not the printer has more jobs in its queue
+	 * 
+	 * @return 
+	 * 			true if printer has at least 1 more job in its queue, otherwise false
+	 */
 	public boolean hasNextJob(){
 		return !jobQueue.isEmpty();
 	}
 	
+	/**
+	 * prints the next job in the queue
+	 */
 	public void printNextJob(){
 	
 		if(isPrinting){
@@ -41,88 +64,99 @@ public class Printer {
 		}else{
 			Job printingJob = jobQueue.peek();
 			if(printingJob != null){
-				printingJob.setStartTime(time);
-				printingJob.setCompletedTime(time+ ((printingJob.pages-1)/10)+1);
+				printingJob.startTime = time;
+				printingJob.completedTime = time+ ((printingJob.pages-1)/10)+1;
 				isPrinting = true;
 			}
 		}
 	}
 
-
-
-	
-	
-
-
-
-
-
+	/**
+	 * returns name of printer
+	 * 
+	 * @return  
+	 * 			returns the name of the printer
+	 */
 	public String getName() {
 		return name;
 	}
 
-
-
-
-
-
+	/**
+	 * sets name of printer
+	 * 
+	 * @param name
+	 * 			name the printers name will be set to
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
 
-
-
-
-
-
+	/**
+	 * returns minimum pages of printer
+	 * 
+	 * @return  
+	 * 			returns the minimum pages of printer
+	 */
 	public int getMininumPages() {
 		return mininumPages;
 	}
 
-
-
-
-
-
+	/**
+	 * sets minimum pages of printer
+	 * 
+	 * @param minimumPages
+	 * 			minimum pages the printers name will be set to
+	 */
 	public void setMininumPages(int mininumPages) {
 		this.mininumPages = mininumPages;
 	}
 
-
-
-
-
-
+	/**
+	 * returns maximum pages of printer
+	 * 
+	 * @return  
+	 * 			returns the maximum pages of printer
+	 */
 	public int getMaximumPages() {
 		return maximumPages;
 	}
 
-
-
-
-
-
+	/**
+	 * sets maximum pages of printer
+	 * 
+	 * @param maximumPages
+	 * 			maximum pages the printers name will be set to
+	 */
 	public void setMaximumPages(int maximumPages) {
 		this.maximumPages = maximumPages;
 	}
 
-
+	/**
+	 * returns static time variable
+	 * 
+	 * @return  
+	 * 			returns the static time variable
+	 */
 	public static int getTime() {
 		return time;
 	}
 	
+	/**
+	 * sets static time viriable
+	 * 
+	 * @param newTime
+	 * 			time for static time variable to be set to
+	 */
 	public static void setTime(int newTime){
 		time = newTime;
 	}
-
-
+	
+	/**
+	 * incriments static time variable
+	 */
 	public static void incrimentTime() {
 		time++;
 	}
-
-
-
-
 
 	private static class Job{
 		private int pages;
@@ -130,6 +164,11 @@ public class Printer {
 		private int startTime;
 		private int completedTime;
 		
+		/**
+		 * Constructor.  instantiates Job object with provided parameters.  stat time and complete time set to 0
+		 * @param pages
+		 * 		number of pages of job
+		 */
 		public Job(int pages){
 			this.pages = pages;
 			submitTime = time;
@@ -137,38 +176,11 @@ public class Printer {
 			completedTime = 0;
 		}
 
-		public int getPages() {
-			return pages;
-		}
-
-		public void setPages(int pages) {
-			this.pages = pages;
-		}
-
-		public int getSubmitTime() {
-			return submitTime;
-		}
-
-		public void setSubmitTime(int submitTime) {
-			this.submitTime = submitTime;
-		}
-
-		public int getStartTime() {
-			return startTime;
-		}
-
-		public void setStartTime(int startTime) {
-			this.startTime = startTime;
-		}
-
-		public int getCompletedTime() {
-			return completedTime;
-		}
-
-		public void setCompletedTime(int completedTime) {
-			this.completedTime = completedTime;
-		}
-		
+		/**
+		 * returns string representation of job
+		 * @return	
+		 * 		returns string representation of job
+		 */
 		public String toString(){
 			return "Number of Pages: "+pages+"    Submit time: " +submitTime+"     Start Time: "+startTime+"     Completed Time: "+completedTime; 
 		}
--- HW6 - PrinterProject/src/edu/miracosta/cs113/PrinterDriver.java
+++ HW6 - PrinterProject/src/edu/miracosta/cs113/PrinterDriver.java
@@ -1,12 +1,23 @@
 package edu.miracosta.cs113;
-
-import java.security.KeyStore.Entry;
 import java.util.Scanner;
+/**
+ * PrinterDriver.java : Program for testing for printer class with queues
+ * 
+ * @author Michael Jones
+ * @version 1.0
+ *
+ */
 
 public class PrinterDriver {
 	private static int[] testArray;
 	private static int MAX_PAGES = 50;
 
+	/**
+	 * Driver for printer program
+	 * 
+	 * @param args
+	 *            command line arguments (unnused)
+	 */
 	public static void main(String[] args){
 		String numPrinters = "";
 		boolean isEntering = true;
@@ -26,6 +37,7 @@ public class PrinterDriver {
 				System.out.println("!!!!!!!!!!Invalid entry!!!!!!");
 			}
 		}
+		keyboard.close();
 
 		if(numPrinters.equals("3")){
 			for(int i = 0;i<testArray.length;i++){
@@ -70,6 +82,9 @@ public class PrinterDriver {
 		}
 	}
 	
+	/**
+	 * generates array of random integers between 1 and MAX_PAGES
+	 */
 	private static void generateTestArray(int size){
 		testArray = new int[size];
 		for(int i=0; i<testArray.length;i++){

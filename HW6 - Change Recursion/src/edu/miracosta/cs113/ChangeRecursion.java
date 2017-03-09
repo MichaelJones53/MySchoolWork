9a7f20bc14def15eb13b470d8c160cb267377f9e
Michael Jones <mykejonez53@gmail.com>
2017-03-09 11:20:29 -0800

    Initial Commit HW6 - Change Recursion
    Created change recursion folder and file structure.

---
+++ HW6 - Change Recursion/src/edu/miracosta/cs113/ChangeRecursion.java
@@ -0,0 +1,20 @@
+package edu.miracosta.cs113;
+
+public class ChangeRecursion {
+
+	public static void main(String[] args) {
+		
+
+		
+	}
+/*
+ * Write	a	recursive	method	that	will	dispense	change	for	a	given	
+amount	of	money.		The	method	will	display all	combinations	of	quarters,	
+dimes,	nickels,	and	pennies	that	equal	the	desired	amount.		Avoid	
+duplication	if	possible.
+NOTE: Your	program	should	dispense	the	highest	coin	first	(quarters,	then	
+dimes,	then	nickels, then	pennies).		Provide	tests	for	5	cent	increments	
+between	5	cents	and	30	cents,	and	a	larger	test	for	75	cents	(hint:	75	cents	
+has	121	unique	number	of	combinations)
+ */
+}
---
+++ HW6 - Change Recursion/src/edu/miracosta/cs113/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Contains class for HW 6 Change Recursion exercise
+ */
+/**
+ * @author W7201584
+ *
+ */
+package edu.miracosta.cs113;

4e2c95b281a3d410b59d8c9a928e9cea489a0a3c
Michael Jones <mykejonez53@gmail.com>
2017-03-11 19:11:18 -0800

    Commit Assembler
    Commit Assembler.  added methods to symboltable and code.  added
    documentation as well.

--- Assembler/src/Code.java
+++ Assembler/src/Code.java
@@ -8,11 +8,15 @@ public class Code {
 	private HashMap<String, String> destCodes;
 	private HashMap<String, String> jumpCodes;
 	
+	//DESCRIPTION:		initializes hashmpas with binary codes for easy lookup
+	//PRECONDITION:		comp codes = 7 bits (includes a), dest/jump codes = 3 bits
+	//POSTCONDITION:	all hashmaps have lookups for valid codes
 	public Code(){
 		compCodes = new HashMap<String, String>();
 		destCodes = new HashMap<String, String>();
 		jumpCodes = new HashMap<String, String>();
-		
+		//comp codes
+		//a=0
 		compCodes.put("0", "0101010");
 		compCodes.put("1", "0111111");
 		compCodes.put("-1", "0111010");
@@ -33,11 +37,67 @@ public class Code {
 		compCodes.put("D-A", "0010011");
 		compCodes.put("A-D", "0000111");
 		compCodes.put("D&A", "0000000");
-		//should i have an A&D?
 		compCodes.put("D|A", "0010101");
-		
-		
-		
-		
+		//a=1
+		compCodes.put("M", "1110000");
+		compCodes.put("!M", "1110001");
+		compCodes.put("-M", "1110011");
+		compCodes.put("M+1", "1110111");
+		compCodes.put("1+M", "1110111");
+		compCodes.put("M-1", "1110010");
+		compCodes.put("M+D", "1000010");
+		compCodes.put("D-M", "1010011");
+		compCodes.put("M-D", "1000111");
+		compCodes.put("D&M", "1000000");
+		compCodes.put("D|M", "1010101");
+		//dest codes
+		destCodes.put(null, "000");
+		destCodes.put("M", "001");
+		destCodes.put("D", "010");
+		destCodes.put("MD", "011");
+		destCodes.put("DM", "011");
+		destCodes.put("A", "100");
+		destCodes.put("AM", "101");
+		destCodes.put("MA", "101");
+		destCodes.put("AD", "110");
+		destCodes.put("AD", "110");
+		destCodes.put("AMD", "111");
+		destCodes.put("ADM", "111");
+		destCodes.put("MAD", "111");
+		destCodes.put("MDA", "111");
+		destCodes.put("DAM", "111");
+		destCodes.put("DMA", "111");
+		//jump codes
+		jumpCodes.put(null, "000");
+		jumpCodes.put("JGT", "001");
+		jumpCodes.put("JEQ", "010");
+		jumpCodes.put("JGE", "011");
+		jumpCodes.put("JLT", "100");
+		jumpCodes.put("JNE", "101");
+		jumpCodes.put("JLE", "110");
+		jumpCodes.put("JMP", "111");
 	}
+	
+	//DESCRIPTION:		converts to string of bits (7) for given mneomic
+	//PRECONDITION:		hashmaps are built with valid values
+	//POSTCONDITION:	returns string of bits if valid.  else returns null
+	public String getComp(String mnemonic){
+		return compCodes.get(mnemonic);
+	}
+	
+	//DESCRIPTION:		converts to string of bits (3) for given mneomic
+	//PRECONDITION:		hashmaps are built with valid values
+	//POSTCONDITION:	returns string of bits if valid.  else returns null
+	public String getDest(String mnemonic){
+		return destCodes.get(mnemonic);
+	}
+	
+	//DESCRIPTION:		converts to string of bits (3) for given mneomic
+	//PRECONDITION:		hashmaps are built with valid values
+	//POSTCONDITION:	returns string of bits if valid.  else returns null
+	public String getJump(String mnemonic){
+		return jumpCodes.get(mnemonic);
+	}
+	
+	
 }
--- Assembler/src/SymbolTable.java
+++ Assembler/src/SymbolTable.java
@@ -1,4 +1,42 @@
 
 public class SymbolTable {
 
+	//DESCRIPTION:		initializes hashmap with predefined symbols
+	//PRECONDITION:		follows symbol/values from book/appendix
+	//POSTCONDITION:	all hashmaps values have valid address integer
+	public SymbolTable(){
+		
+	}
+	//DESCRIPTION:		adds new pair of symbol/address to hashmap	
+	//PRECONDITION:		symbol/address pair not in hashmap (check contains() 1st)
+	//POSTCONDITION:	adds pair, returns true if addes, false if illegal name
+	public boolean addEntry(String symbol, int address){
+		//STUB
+		return false;
+	}
+	
+	//DESCRIPTION:		returns boolean of whether hashmap has symbol or not	
+	//PRECONDITION:		table has been initalized
+	//POSTCONDITION:	returns boolean if arg is in table or not
+	public boolean contains(String symbol){
+		//STUB
+		return false;
+	}
+	
+	//DESCRIPTION:		returns address in hashmap of given symbol
+	//PRECONDITION:		symbol is in hashmap (check w/ contains() first)
+	//POSTCONDITION:	returns address associated with symbol in hashmap
+	public int getAddress(String symbol){
+		//stub
+		return -1;
+	}
+	
+	
+	public boolean isValidName(String symbol){
+		//stub
+		return false;
+	}
+	
+	
+	
 }

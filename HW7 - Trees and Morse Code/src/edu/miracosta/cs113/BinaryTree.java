ef5f156a16af1e83bf7004dd6d98fec8f888af2a
Michael Jones <mykejonez53@gmail.com>
2017-04-08 16:43:54 -0700

    Commit HW 7
    Completed HW7.

--- HW7 - Trees and Morse Code/src/edu/miracosta/cs113/BinaryTree.java
+++ HW7 - Trees and Morse Code/src/edu/miracosta/cs113/BinaryTree.java
@@ -9,7 +9,7 @@ public class BinaryTree<E> implements Serializable {
 	protected Node<E> root;
 
 	/**
-	 * No argument constructor.  instantiates BinaryTree object
+	 * No argument constructor. instantiates BinaryTree object
 	 */
 	public BinaryTree() {
 		root = null;
@@ -19,21 +19,22 @@ public class BinaryTree<E> implements Serializable {
 	 * Constructs binary tree with given node as the root
 	 * 
 	 * @param root
-	 * 		node to be used as trees root
+	 *            node to be used as trees root
 	 */
 	protected BinaryTree(Node<E> root) {
 		this.root = root;
 	}
 
 	/**
-	 * Constructs binary tree with given data as the root and the two given subtrees
+	 * Constructs binary tree with given data as the root and the two given
+	 * subtrees
 	 * 
 	 * @param data
-	 * 			data to be set in root node
+	 *            data to be set in root node
 	 * @param leftTree
-	 * 			Binary tree to be used as root nodes left subtree
+	 *            Binary tree to be used as root nodes left subtree
 	 * @param rightTree
-	 * 			Binary tree to be used as root nodes right subtree
+	 *            Binary tree to be used as root nodes right subtree
 	 */
 	public BinaryTree(E data, BinaryTree<E> leftTree, BinaryTree<E> rightTree) {
 		root = new Node<E>(data);
@@ -52,8 +53,8 @@ public class BinaryTree<E> implements Serializable {
 	/**
 	 * gets the trees left subtree
 	 *
-	 * @return
-	 * 		Returns the trees left subtree. returns null if left subtree is empty 
+	 * @return Returns the trees left subtree. returns null if left subtree is
+	 *         empty
 	 */
 	public BinaryTree<E> getLeftSubtree() {
 		if (root != null && root.left != null) {
@@ -66,8 +67,8 @@ public class BinaryTree<E> implements Serializable {
 	/**
 	 * gets the trees right subtree
 	 *
-	 * @return
-	 * 		Returns the trees right subtree. returns null if right subtree is empty 
+	 * @return Returns the trees right subtree. returns null if right subtree is
+	 *         empty
 	 */
 	public BinaryTree<E> getRightSubtree() {
 		if (root != null && root.right != null) {
@@ -80,8 +81,7 @@ public class BinaryTree<E> implements Serializable {
 	/**
 	 * gets the data from the root node
 	 *
-	 * @return
-	 * 		Returns the root nodes data.   returns null if node is null
+	 * @return Returns the root nodes data. returns null if node is null
 	 */
 	public E getData() {
 		if (root != null) {
@@ -94,8 +94,7 @@ public class BinaryTree<E> implements Serializable {
 	/**
 	 * identifies if the node is a leaf node
 	 * 
-	 * @return
-	 * 		returns true if node is a leaf node.  otherwise, returns false
+	 * @return returns true if node is a leaf node. otherwise, returns false
 	 */
 	public boolean isLeaf() {
 		return (root.left == null && root.right == null);
@@ -104,8 +103,7 @@ public class BinaryTree<E> implements Serializable {
 	/**
 	 * provides preorder traversal string representation of entire binary tree
 	 * 
-	 * @return
-	 * 		returns preorder traversal string representation of binary tree
+	 * @return returns preorder traversal string representation of binary tree
 	 */
 	public String toString() {
 		StringBuilder sb = new StringBuilder();
@@ -114,22 +112,23 @@ public class BinaryTree<E> implements Serializable {
 	}
 
 	/**
-	 * performs preorder traversal of tree recursively.  
-	 * @param node	
-	 * 		root of tree being traversed
+	 * performs preorder traversal of tree recursively.
+	 * 
+	 * @param node
+	 *            root of tree being traversed
 	 * 
 	 * @param depth
-	 * 		current level of tree
+	 *            current level of tree
 	 * 
 	 * @param sb
-	 * 		string being appended to be returned as string representation of tree
+	 *            string being appended to be returned as string representation
+	 *            of tree
 	 * 
-	 * @return
-	 * 		returns sb as preordered traversal string representation of tree 
+	 * @return returns sb as preordered traversal string representation of tree
 	 */
 	private void preOrderTraverse(Node<E> node, int depth, StringBuilder sb) {
 		for (int i = 1; i < depth; i++) {
-			sb.append(" ");
+			sb.append("  ");
 		}
 		if (node == null) {
 			sb.append("null\n");
@@ -142,32 +141,31 @@ public class BinaryTree<E> implements Serializable {
 
 	/**
 	 * Constructs binary tree by reading data from scanner
+	 * 
 	 * @param scan
-	 * 		scanner being read from
-	 * @return
-	 * 		Binary tree created from scanner
+	 *            scanner being read from
+	 * @return Binary tree created from scanner
 	 */
 	public static BinaryTree<String> readBinaryTree(Scanner scan) {
 		String data = scan.next();
 		if (data.equals("null")) {
+			
 			return null;
 		} else {
 			BinaryTree<String> leftTree = readBinaryTree(scan);
 			BinaryTree<String> rightTree = readBinaryTree(scan);
+
 			return new BinaryTree<String>(data, leftTree, rightTree);
 		}
 	}
-	
-	
 
 	protected static class Node<E> implements Serializable {
 		protected E data;
 		protected Node<E> left;
 		protected Node<E> right;
 
-
 		/**
-		 * No argument constructor.  instantiates Node object
+		 * No argument constructor. instantiates Node object
 		 */
 		public Node(E data) {
 			this.data = data;
@@ -176,10 +174,10 @@ public class BinaryTree<E> implements Serializable {
 		}
 
 		/**
-		 * provides String representation of data in node.  assumes node is not null
+		 * provides String representation of data in node. assumes node is not
+		 * null
 		 * 
-		 * @return
-		 * 		returns string representation of data in node
+		 * @return returns string representation of data in node
 		 */
 		public String toString() {
 			return data.toString();
--- HW7 - Trees and Morse Code/src/edu/miracosta/cs113/MorseCodeProject.java
+++ HW7 - Trees and Morse Code/src/edu/miracosta/cs113/MorseCodeProject.java
@@ -2,26 +2,214 @@ package edu.miracosta.cs113;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class MorseCodeProject {
 
+	private static BinaryTree<String> morseTree = new BinaryTree<String>();
+
 	public static void main(String[] args) {
 		String filename = "C:\\Users\\W7201584\\Desktop\\morse.txt";
-		BinaryTree<String> morseTree = new BinaryTree<String>();
+
+		boolean isRunning = true;
+		Scanner keyboard = new Scanner(System.in);
+		Scanner scanner;
+		FileInputStream instream;
+		int selection = 0;
 
 		try {
-			FileInputStream instream = new FileInputStream(filename);
-			Scanner scanner = new Scanner(instream);
+			instream = new FileInputStream(filename);
+			scanner = new Scanner(instream);
 			while (scanner.hasNext()) {
 				morseTree = BinaryTree.readBinaryTree(scanner);
 
 			}
+			scanner.close();
 		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
+			System.exit(1);
+		}
+
+		while (isRunning) {
+			boolean isSelecting = true;
+			System.out.println("");
+			System.out.println("*************************************************");
+			System.out.println("1 - display morse code table");
+			System.out.println("2 - enter file path to translte from morse code to text");
+			System.out.println("3 - enter text in console to translate to morse code");
+			System.out.println("4 - Exit");
+			System.out.println("*************************************************");
+
+			while (isSelecting) {
+
+				try {
+					selection = keyboard.nextInt();
+					if (selection > 4 || selection < 1) {
+						throw new InputMismatchException();
+					}
+					isSelecting = false;
+				} catch (InputMismatchException e) {
+					System.out.println("Invalid entry.  try agian");
+				} finally {
+					keyboard.nextLine();
+				}
+			}
+
+			if (selection == 1) {
+				displayAsTable((treeAsTable(morseTree)));
+
+			} else if (selection == 2) {
+				System.out.println("enter the full file path to be translated");
+				String userFileName = keyboard.nextLine();
+				try{
+					instream = new FileInputStream(userFileName);
+					scanner = new Scanner(instream);
+					while(scanner.hasNext()){
+						System.out.print(translateFromMorseToText(scanner.next().toLowerCase(), morseTree));
+					}
+					scanner.close();
+				}catch(FileNotFoundException e){
+					System.out.println("file not found.  Returnning to main menu");
+				}
+
+			} else if (selection == 3) {
+
+				String userString = null;
+				System.out.println("Enter line of text to traslate.  numbers and special characers will be ignored!");
+				userString = keyboard.nextLine();
+				System.out.println(translateText(userString.toLowerCase()));
+
+			} else {
+				isRunning = false;
+			}
+		}
+
+		System.out.println("Toodles!");
+	}
+
+	/**
+	 * generates table to view binary tree data
+	 * 
+	 * @param tree
+	 * 			Binary tree to generate table from
+	 * @return
+	 * 		String representation as data table of Binary Tree
+	 */
+	private static String treeAsTable(BinaryTree tree) {
+		if (tree.isLeaf()) {
+			return tree.getData() + "\n";
+		} else {
+			if (tree.getRightSubtree() == null) {
+				return tree.getData() + "\n" + treeAsTable(tree.getLeftSubtree());
+			}
+			if (tree.getLeftSubtree() == null) {
+				return tree.getData() + "\n" + treeAsTable(tree.getRightSubtree());
+			}
+			return tree.getData() + "\n" + treeAsTable(tree.getLeftSubtree()) + treeAsTable(tree.getRightSubtree());
+
 		}
 
 	}
 
+	/**
+	 * sorts and displays passed string as alphabetical table 5 columns in breadth
+	 * 
+	 * @param data
+	 * 		String to be dislayed as a table
+	 */
+	private static void displayAsTable(String data) {
+		ArrayList<String> dataAsList = new ArrayList<String>();
+		Scanner scanner = new Scanner(data);
+		while (scanner.hasNext()) {
+			String newData = scanner.next();
+			if (!newData.equalsIgnoreCase("0")) {
+				newData = newData.substring(0, 1) + "  " + newData.substring(1, newData.length());
+				dataAsList.add(newData);
+			}
+		}
+		Collections.sort(dataAsList);
+		for (int i = 0; i < dataAsList.size(); i++) {
+			if (i % 5 == 0) {
+				System.out.println("");
+			}
+			System.out.print(dataAsList.get(i) + "\t");
+
+		}
+	}
+
+	/**
+	 * converts alphabetical text to morse code
+	 * 
+	 * @param data
+	 * 			string to be coverted	
+	 * @return
+	 * 		morse code representation of provided data
+	 */
+	private static String translateText(String data) {
+
+		return stringToMorse(data, 0);
+	}
+
+	private static String stringToMorse(String data, int position) {
+		if (position == data.length()) {
+			return "";
+		} else {
+			if (data.charAt(position) == ' ') {
+				return " " + stringToMorse(data, position + 1);
+			} else {
+
+				return searchBinaryTree(data.charAt(position), morseTree) + " " + stringToMorse(data, position + 1);
+
+			}
+		}
+
+	}
+
+	/**
+	 * Searches binary tree for provided character and provides morse representation
+	 * 
+	 * @param item
+	 * 			character being converted to morse
+	 * @param tree
+	 * 			tree being searched for character
+	 * @return
+	 * 			returns the character in morse representation.  returns empty string if not found
+	 */
+	private static String searchBinaryTree(char item, BinaryTree<String> tree) {
+		if (tree == null) {
+			return "";
+		} else if (tree.getData().toLowerCase().charAt(0) == item) {
+
+			return tree.getData().substring(1);
+		} else {
+			return searchBinaryTree(item, tree.getLeftSubtree()) + searchBinaryTree(item, tree.getRightSubtree());
+
+		}
+		
+	}
+	
+	/**
+	 * translates morse code to alphabetical text
+	 * @param data
+	 * 			data to be translated
+	 * @param tree
+	 * 			tree to be searched
+	 * @return
+	 * 			returns alphabetical representation of provided morse string
+	 */
+	private static char translateFromMorseToText(String data, BinaryTree<String> tree){
+		if(data.length() == 0){
+			return tree.getData().charAt(0);
+		}else{
+			if(data.charAt(0) == '*'){
+				return translateFromMorseToText(data.substring(1), tree.getLeftSubtree());
+			}else{
+				return translateFromMorseToText(data.substring(1), tree.getRightSubtree());
+			}
+		}
+		
+	}
 }

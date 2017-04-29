cf2cfd4a3edc307033bf739f127e2c937723ddcc
Michael Jones <mykejonez53@gmail.com>
2017-04-29 09:42:56 -0700

    Commit Lab 16 - Radix sort
    Commit Lab 16.  Completed.  This actually works for all positive
    integers of any value.

--- Lab 16 - Radix Sort/src/edu/miracosta/cs113/Main.java
+++ Lab 16 - Radix Sort/src/edu/miracosta/cs113/Main.java
@@ -1,17 +1,20 @@
 package edu.miracosta.cs113;
 
 public class Main {
-	public static void main(String[] args) throws InterruptedException{
-		
-		int[] test = {2,13,1,4,7,5};
-	
-		test = Sort.radixSort(test);
-	
-		for(int i = 0; i< test.length; i++){
-			System.out.println(test[i]);
+	public static void main(String[] args) {
+
+		int[] test = new int[50];
+
+		for (int i = 0; i < 50; i++) {
+
+			test[i] = (int) (Math.random() * 1000);
 		}
-		
-		
+		test = Sort.radixSort(test);
+
+		for (int i = 0; i < test.length; i++) {
+			System.out.println((i + 1) + ") " + test[i]);
+		}
+
 	}
 
 }
--- Lab 16 - Radix Sort/src/edu/miracosta/cs113/Sort.java
+++ Lab 16 - Radix Sort/src/edu/miracosta/cs113/Sort.java
@@ -2,65 +2,94 @@ package edu.miracosta.cs113;
 
 public class Sort {
 
-	public static int[] radixSort(int[] data) throws InterruptedException {
+	/**
+	 * precondition - Data is assumed to be >= 0 and integers 
+	 * Sorts array of positive integers 
+	 * @param data
+	 * 		the data to be sorted
+	 * @return
+	 * 		sorted array of the provided data
+	 */
+	public static int[] radixSort(int[] data) {
 		int[] counter = new int[10];
 		int[] transfer = new int[data.length];
 		int maxValue = 0;
-		int pass = 1;
-		
-		
-		for(int i = 0; i< data.length; i++){
-			if(data[i]> maxValue){
+		int pass = 0;
+
+		for (int i = 0; i < data.length; i++) {
+			if (data[i] > maxValue) {
 				maxValue = data[i];
 			}
 		}
-		
+
 		return redistribute(data, counter, transfer, pass, maxValue);
 	}
 
-	/************** private helper methods 
-	 * @throws InterruptedException **************/
-	private static int[] redistribute(int[] data, int[] counter, int[] transfer, int pass, int maxVal) throws InterruptedException {
+	
+	/**
+	 * Helper method to recursively provress through dataset and continue sorting using radix algorithm
+	 * @param data
+	 * 		the data to be sorted
+	 * @param counter
+	 * 		array that stores the number of times a digit appears in the set
+	 * @param transfer
+	 * 		array to store the sorted data
+	 * @param pass
+	 * 		keeps track of which pass the recursive method is on
+	 * @param maxVal
+	 * 		largest value in the data.  needed for ending recursive calls
+	 * @return
+	 * 		returns the sorted data
+	 */
+	private static int[] redistribute(int[] data, int[] counter, int[] transfer, int pass, int maxVal) {
 
 		resetCounter(counter);
-		countDigits(data, counter);
-		
+		countDigits(data, counter, pass);
+
 		for (int i = 0; i < counter.length - 1; i++) {
 			counter[i + 1] += counter[i];
 		}
-		
+
 		for (int i = data.length - 1; i >= 0; i--) {
 			int value = data[i];
-			int modResults = (value/(int)Math.pow(10, pass-1)) % 10;
+			int modResults = (value / (int) Math.pow(10, pass)) % 10;
 			int destination = counter[modResults] - 1;
-			System.out.println(counter[modResults]);
-			System.out.println(destination);
 			transfer[destination] = value;
 			counter[modResults]--;
 
 		}
-		if((maxVal % (int)Math.pow(10, pass)) != maxVal){
-			System.out.println(maxVal);
-			System.out.println((int)Math.pow(10, pass));
-			System.out.println(maxVal % (int)Math.pow(10, pass));
-			Thread.sleep(500);
-			pass = pass+1;
+		if ((maxVal % (int) Math.pow(10, pass)) != maxVal) {
+			pass++;
 			return redistribute(transfer, counter, data, pass, maxVal);
-		}else{
+		} else {
 			return transfer;
 		}
 
 	}
 
+	/**
+	 * Helper method to reset the coutner array to 0
+	 * @param data
+	 * 		the counter array to be reset
+	 */
 	private static void resetCounter(int[] data) {
 		for (int i = 0; i < data.length; i++) {
 			data[i] = 0;
 		}
 	}
 
-	private static void countDigits(int[] data, int[] counter) {
+	/**
+	 * Helper method that reads through the data and counts the orrurance of the digits in the desired place
+	 * @param data
+	 * 		data to be searched through
+	 * @param counter
+	 * 		array that keeps track of the orrurances of the digit
+	 * @param pass
+	 * 		the number of sorting attempts conducted
+	 */
+	private static void countDigits(int[] data, int[] counter, int pass) {
 		for (int i = 0; i < data.length; i++) {
-			int counterLocation = data[i] % 10;
+			int counterLocation = (data[i] / (int) Math.pow(10, pass)) % 10;
 			counter[counterLocation]++;
 
 		}
